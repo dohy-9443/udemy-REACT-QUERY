9dc649cda64a8ce911ad7d630c96689dd68519e0
dohy-9443 <79897962+dohy-9443@users.noreply.github.com>
2022-07-03 23:43:19 +0900

    1챕터 강의 완료 react-query 기초 isError , isLoading , error , isFetching
      // isError : 어떤 데이터를 가저올 때 오류가 있는지 여부
      // isLoading : 데이터가 로딩 중인지 여부 ( 데이터를 가져오는 중이고 표시할 캐시 데이터도 없다. )
      // isFetching : 비동기 쿼리가 해결되지 않았음을 의미

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -15,9 +15,17 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery("posts", fetchPosts)
+  const { data, isError, error, isLoading, isFetching } = useQuery("posts", fetchPosts, {
+    staleTime: 2000,
+  });
 
-  if (!data) return <div />;
+  // isError : 어떤 데이터를 가저올 때 오류가 있는지 여부
+  // isLoading : 데이터가 로딩 중인지 여부 ( 데이터를 가져오는 중이고 표시할 캐시 데이터도 없다. )
+  // isFetching : 비동기 쿼리가 해결되지 않았음을 의미
+
+  if ( isLoading ) return <h3>loading...</h3>;
+
+  if ( isError ) return <><h3>에러남</h3><p>{error.toString()}</p></>;
 
   return (
     <>
