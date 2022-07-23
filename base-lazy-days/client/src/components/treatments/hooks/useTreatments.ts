f4f4f47af0285e64c623c4a1e42b110c3916eb79
dohy-9443 <79897962+dohy-9443@users.noreply.github.com>
2022-07-23 23:34:20 +0900

    Feat: useIsFetching을 사용하는 중앙 집중식 페칭 표시기(Indicator) , useQuery에 대한 onError 핸들러 , 쿼리 클라이언트에 대한 onError 기본 값

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 
@@ -15,9 +15,23 @@ async function getTreatments(): Promise<Treatment[]> {
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
 
+  // const toast = useCustomToast();
+
   const fallback = [];
 
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(
+    queryKeys.treatments,
+    getTreatments,
+    // {
+    //   onError: (error) => {
+    //     const title =
+    //       error instanceof Error
+    //         ? error.message
+    //         : 'error connecting to the server';
+    //     toast({ title, status: 'error' });
+    //   },
+    // }
+  );
 
   return data;
 }
